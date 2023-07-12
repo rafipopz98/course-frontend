7d4f94346baf0633aec7160603d7a3b800c83157
rafipopz98 <itsrealrafi@gmail.com>
2023-07-12 21:36:31 +0530

    payemt fail

--- src/components/Payments/PaymentFailure.jsx
+++ src/components/Payments/PaymentFailure.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button, Container, Heading, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { RiErrorWarningFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const PaymentFailure = () => {
   return (
-    <div>PaymentFailure</div>
-  )
-}
+    <Container h="90vh" >
+      
+      <VStack justifyContent={'center'} h={'full'}spacing={'4'}>
+      <RiErrorWarningFill  size={'5rem'}/>
+      <Heading textTransform={'uppercase'} my="8" textAlign={'center'}>
+        Payment fail
+      </Heading>
+        <Link to={'/subscribe'}>
+          <Button varient={'ghost'}>Try Again</Button>
+        </Link>
+        
+      </VStack>
+    </Container>
+  );
+};
 
-export default PaymentFailure
+export default PaymentFailure;
