a488be31e89277603c6c9288ea64d8f6a3a9f1cb
rafipopz98 <itsrealrafi@gmail.com>
2023-07-13 18:50:19 +0530

    dashboard users ...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Header from './components/Layout/Header/Header'
+import Header from './components/Layout/Header/Header';
 import Courses from './components/courses/Courses';
 import Footer from './components/Layout/Footer/Footer';
 import Login from './components/Auth/Login';
@@ -11,46 +11,58 @@ import ResetPassword from './components/Auth/ResetPassword';
 import Contactus from './components/Contactus.jsx/Contactus';
 import Request from './components/Request/Request';
 import About from './components/About/About';
-import NotFound from './components/Layout/NotFound/Notfound'
-import PaymentSuccess from './components/Payments/PaymentSuccess'
-import PaymentFailure from './components/Payments/PaymentFailure'
-import Subscribe  from './components/Payments/Subscribers';
+import NotFound from './components/Layout/NotFound/Notfound';
+import PaymentSuccess from './components/Payments/PaymentSuccess';
+import PaymentFailure from './components/Payments/PaymentFailure';
+import Subscribe from './components/Payments/Subscribers';
 import CoursePage from './components/CoursePage/CoursePage';
-import Profile from './components/Profile/Profile'
+import Profile from './components/Profile/Profile';
 import ChangePassword from './components/Profile/ChangePassword';
 import UpdateProfile from './components/Profile/UpdateProfile';
 import Dashboard from './components/Admin/Dashboard/Dashboard';
-
+import Users from './components/Admin/Users/Users';
+import CreateCourse from './components/Admin/CreateCourse/CreateCourse';
+import Admincourse from './components/Admin/AdminCourses/Admincourse'
 function App() {
-
-  window.addEventListener("contextmenu",(e)=>{
-    e.preventDefault()
-  })
-  return ( 
+  window.addEventListener('contextmenu', e => {
+    e.preventDefault();
+  });
+  return (
     <>
       <Router>
-      <Header />
+        <Header />
         <Routes>
-          <Route  exact path={'/'} element={<Home/>}/>
-          <Route  exact path={'/courses'} element={<Courses/>}/>
-          <Route  exact path={'/course/:id'} element={<CoursePage/>}/>
+          <Route exact path={'/'} element={<Home />} />
+          <Route exact path={'/courses'} element={<Courses />} />
+          <Route exact path={'/course/:id'} element={<CoursePage />} />
 
-          <Route  exact path={'/login'} element={<Login/>}/>
-          <Route  exact path={'/signup'} element={<Signup/>}/>
-          <Route  exact path={'/forgotpassword'} element={<ForgetPassword/>}/>
-          <Route  exact path={'/resetpassword/:token'} element={<ResetPassword/>}/>
-          <Route  exact path={'/contact'} element={<Contactus/>}/>
-          <Route  exact path={'/about'} element={<About/>}/>
-          <Route exact path ={'/profile'} element={<Profile/>}/>
-          <Route  exact path={'/request'} element={<Request/>}/>
-          <Route  exact path={'/subscribe'} element={<Subscribe/>}/>
-          <Route  exact path={'/paymentsuccess'} element={<PaymentSuccess/>}/>
-          <Route  exact path={'/paymentfailure'} element={<PaymentFailure/>}/>
-          <Route  exact path={'*'} element={<NotFound/>}/>
-          <Route  exact path={'/changepassword'} element={<ChangePassword/>}/>
-          <Route  exact path={'/updateprofile'} element={<UpdateProfile/>}/>
-          <Route  exact path={'/admin/dashboard'} element={<Dashboard/>}/>
-          {/* <Route  exact path={'/updateprofile'} element={<UpdateProfile/>}/> */}
+          <Route exact path={'/login'} element={<Login />} />
+          <Route exact path={'/signup'} element={<Signup />} />
+          <Route exact path={'/forgotpassword'} element={<ForgetPassword />} />
+          <Route
+            exact
+            path={'/resetpassword/:token'}
+            element={<ResetPassword />}
+          />
+          <Route exact path={'/contact'} element={<Contactus />} />
+          <Route exact path={'/about'} element={<About />} />
+          <Route exact path={'/profile'} element={<Profile />} />
+          <Route exact path={'/request'} element={<Request />} />
+          <Route exact path={'/subscribe'} element={<Subscribe />} />
+          <Route exact path={'/paymentsuccess'} element={<PaymentSuccess />} />
+          <Route exact path={'/paymentfailure'} element={<PaymentFailure />} />
+          <Route exact path={'*'} element={<NotFound />} />
+          <Route exact path={'/changepassword'} element={<ChangePassword />} />
+          <Route exact path={'/updateprofile'} element={<UpdateProfile />} />
+
+          <Route exact path={'/admin/dashboard'} element={<Dashboard />} />
+          <Route
+            exact
+            path={'/admin/createcourse'}
+            element={<CreateCourse />}
+          />
+          <Route exact path={'/admin/courses'} element={<Admincourse />} />
+          <Route exact path={'/admin/users'} element={<Users />} />
         </Routes>
         <Footer />
       </Router>
